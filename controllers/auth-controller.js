9f313e5aff59aaa764cb7496fa76fb87291c9de6
Garv nanwani <garvwashere@gmail.com>
2022-06-20 21:58:13 +0530

    fixed CORS issue

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -6,172 +6,171 @@ const UserDto = require('../dtos/user-dto');
 
 class AuthController {
 
-    async sendOtp(req, res) {
-        const { phone, email } = req.body;
+  async sendOtp(req, res) {
+    const { phone, email } = req.body;
 
-        const service = phone ? phone : email
+    const service = phone ? phone : email
 
-        if (!service) {
-            return res.status(400).json({ message: "Phone number or Email is required" });
-        }
-
-        if (phone) {
-            if (!phone.match(/^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$/)) {
-                return res.status(400).json({ message: "Invalid phone number" });
-            }
-        }
-
-        const otp = await otpService.generateOtp();
-        const ttl = 1000 * 60 * 5;
-        const expires = Date.now() + ttl;
-        const data = `${service}.${otp}.${expires}`;
-        const hash = hashService.hashOtp(data);
-
-        try {
-            // await otpService.sendBySms(phone, otp);
-            if (email) {
-                await otpService.sendByEmail(email, otp);
-            }
-            res.status(200).json({
-                hash: `${hash}.${expires}`,
-                phone,
-                email,
-                otp
-            });
-        } catch (err) {
-            console.log(err);
-            return res.status(500).json({ message: 'Message sending failed' });
-        }
+    if (!service) {
+      return res.status(400).json({ message: "Phone number or Email is required" });
     }
 
-    async verifyOtp(req, res) {
-        const { otp, hash, phone, email } = req.body;
-
-        const service = phone ? phone : email
-
-        console.log(service)
-
-        if (!otp || !hash || !service) {
-            return res.status(400).json({ message: "Otp is required" })
-        }
-
-        const [hashedOtp, expires] = hash.split('.');
-        if (Date.now() > +expires) {
-            return res.status(400).json({ message: "OTP expired" })
-        }
-
-        const data = `${service}.${otp}.${expires}`;
-
-        const isValid = otpService.verifyOtp(hashedOtp, data);
-
-        if (!isValid) {
-            return res.status(400).json({ message: "Invalid OTP" });
-        }
-
-        let user;
-        try {
-            if (email) {
-                user = await userService.findUser({ email });
-                if (!user) {
-                    user = await userService.createUser({ email });
-                }
-            } else {
-                user = await userService.findUser({ phone });
-                if (!user) {
-                    user = await userService.createUser({ phone });
-                }
-            }
-        } catch (err) {
-            console.log(err);
-            return res.status(500).json({ message: 'DB error' })
-        }
-
-        const { accessToken, refreshToken } = tokenService.generateToken({
-            _id: user._id,
-            activated: false
-        });
-
-        await tokenService.storeRefreshToken(refreshToken, user._id);
-
-        res.cookie("refreshToken", refreshToken, {
-            maxAge: 1000 * 60 * 60 * 24 * 30,
-            httpOnly: true
-        })
-
-        res.cookie("accessToken", accessToken, {
-            maxAge: 1000 * 60 * 60 * 24 * 30,
-            httpOnly: true
-        })
-
-        const userDto = new UserDto(user)
-        res.json({ user: userDto, auth: true });
-
+    if (phone) {
+      if (!phone.match(/^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$/)) {
+        return res.status(400).json({ message: "Invalid phone number" });
+      }
     }
 
-    async refresh(req, res) {
-        const { refreshToken: refreshTokenFromCookies } = req.cookies;
-        let userData;
-        try {
-            userData = await tokenService.verifyRefreshToken(refreshTokenFromCookies);
+    const otp = await otpService.generateOtp();
+    const ttl = 1000 * 60 * 5;
+    const expires = Date.now() + ttl;
+    const data = `${service}.${otp}.${expires}`;
+    const hash = hashService.hashOtp(data);
 
-        } catch (err) {
-            return res.status(401).json({ message: "Invalid refresh token" });
-        }
+    try {
+      // await otpService.sendBySms(phone, otp);
+      if (email) {
+        await otpService.sendByEmail(email, otp);
+      }
+      res.status(200).json({
+        hash: `${hash}.${expires}`,
+        phone,
+        email,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: 'Message sending failed' });
+    }
+  }
 
-        try {
-            const token = await tokenService.findRefreshToken(userData._id, refreshTokenFromCookies);
-            if (!token) {
-                return res.status(401).json({ message: "Invalid token" });
-            }
-        } catch (err) {
-            return res.status(401).json({ message: "Internal error" });
-        }
+  async verifyOtp(req, res) {
+    const { otp, hash, phone, email } = req.body;
 
-        // for valid user
-        const user = await userService.findUser({ _id: userData._id });
+    const service = phone ? phone : email
 
+    console.log(service)
+
+    if (!otp || !hash || !service) {
+      return res.status(400).json({ message: "Otp is required" })
+    }
+
+    const [hashedOtp, expires] = hash.split('.');
+    if (Date.now() > +expires) {
+      return res.status(400).json({ message: "OTP expired" })
+    }
+
+    const data = `${service}.${otp}.${expires}`;
+
+    const isValid = otpService.verifyOtp(hashedOtp, data);
+
+    if (!isValid) {
+      return res.status(400).json({ message: "Invalid OTP" });
+    }
+
+    let user;
+    try {
+      if (email) {
+        user = await userService.findUser({ email });
         if (!user) {
-            return res.status(401).json({ message: "Invalid user" });
+          user = await userService.createUser({ email });
         }
-        const { refreshToken, accessToken } = await tokenService.generateToken({ _id: userData._id });
-
-        // update refresh token
-        try {
-            await tokenService.updateRefreshToken(userData._id, refreshToken);
+      } else {
+        user = await userService.findUser({ phone });
+        if (!user) {
+          user = await userService.createUser({ phone });
         }
-        catch (err) {
-            return res.status(500).json({ message: "Internal error" });
-        }
-
-        //put in cookies
-        res.cookie("refreshToken", refreshToken, {
-            httpOnly: true,
-            maxAge: 1000 * 60 * 60 * 24 * 30
-        });
-        res.cookie("accessToken", accessToken, {
-            httpOnly: true,
-            maxAge: 1000 * 60 * 60 * 24 * 30
-        });
-        //response
-        const userDto = new UserDto(user);
-        res.json({ user: userDto, auth: true })
-
+      }
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: 'DB error' })
     }
 
-    async logout(req, res) {
-        const { refreshToken } = req.cookies;
-        if (!refreshToken) {
-            return res.status(400).json({ message: "Invalid token" });
-        }
-        try {
-            await tokenService.removeToken(refreshToken);
-        } catch (err) {
-            return res.status(500).json({ message: "Internal error" });
-        }
-        res.clearCookie("refreshToken");
-        res.clearCookie("accessToken");
-        res.status(200).json({ user: null, auth: false });
+    const { accessToken, refreshToken } = tokenService.generateToken({
+      _id: user._id,
+      activated: false
+    });
+
+    await tokenService.storeRefreshToken(refreshToken, user._id);
+
+    res.cookie("refreshToken", refreshToken, {
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      httpOnly: true
+    })
+
+    res.cookie("accessToken", accessToken, {
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      httpOnly: true
+    })
+
+    const userDto = new UserDto(user)
+    res.json({ user: userDto, auth: true });
+
+  }
+
+  async refresh(req, res) {
+    const { refreshToken: refreshTokenFromCookies } = req.cookies;
+    let userData;
+    try {
+      userData = await tokenService.verifyRefreshToken(refreshTokenFromCookies);
+
+    } catch (err) {
+      return res.status(401).json({ message: "Invalid refresh token" });
     }
+
+    try {
+      const token = await tokenService.findRefreshToken(userData._id, refreshTokenFromCookies);
+      if (!token) {
+        return res.status(401).json({ message: "Invalid token" });
+      }
+    } catch (err) {
+      return res.status(401).json({ message: "Internal error" });
+    }
+
+    // for valid user
+    const user = await userService.findUser({ _id: userData._id });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid user" });
+    }
+    const { refreshToken, accessToken } = await tokenService.generateToken({ _id: userData._id });
+
+    // update refresh token
+    try {
+      await tokenService.updateRefreshToken(userData._id, refreshToken);
+    }
+    catch (err) {
+      return res.status(500).json({ message: "Internal error" });
+    }
+
+    //put in cookies
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 30
+    });
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 30
+    });
+    //response
+    const userDto = new UserDto(user);
+    res.json({ user: userDto, auth: true })
+
+  }
+
+  async logout(req, res) {
+    const { refreshToken } = req.cookies;
+    if (!refreshToken) {
+      return res.status(400).json({ message: "Invalid token" });
+    }
+    try {
+      await tokenService.removeToken(refreshToken);
+    } catch (err) {
+      return res.status(500).json({ message: "Internal error" });
+    }
+    res.clearCookie("refreshToken");
+    res.clearCookie("accessToken");
+    res.status(200).json({ user: null, auth: false });
+  }
 }
 
 module.exports = new AuthController();
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const router = require('./routes/routes');
 const connectDB = require('./database');
 const cors = require('cors');
@@ -11,15 +11,15 @@ const ACTIONS = require('./actions');
 const server = require('http').createServer(app);
 
 const io = require('socket.io')(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST']
-    }
+  cors: {
+    origin: process.env.FRONT_URL,
+    methods: ['GET', 'POST']
+  }
 })
 
 const corsOptions = {
-    credentials: true,
-    origin: 'http://localhost:3000'
+  credentials: true,
+  origin: process.env.FRONT_URL
 }
 
 connectDB();
@@ -30,7 +30,7 @@ app.use(express.json({ limit: '8mb' }));
 app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+  res.send('Hello World');
 })
 
 app.use('/api/', router);
@@ -38,98 +38,98 @@ app.use('/api/', router);
 const socketUserMap = {};
 
 io.on('connection', (socket) => {
-    console.log('New connection', socket.id);
-    socket.on(ACTIONS.JOIN, ({ roomId, user }) => {
-        socketUserMap[socket.id] = user;
-        const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
-        clients.forEach((clientId) => {
-            io.to(clientId).emit(ACTIONS.ADD_PEER, {
-                peerId: socket.id,
-                createOffer: false,
-                user,
-            });
-            socket.emit(ACTIONS.ADD_PEER, {
-                peerId: clientId,
-                createOffer: true,
-                user: socketUserMap[clientId],
-            });
-        });
-        socket.join(roomId);
+  console.log('New connection', socket.id);
+  socket.on(ACTIONS.JOIN, ({ roomId, user }) => {
+    socketUserMap[socket.id] = user;
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+    clients.forEach((clientId) => {
+      io.to(clientId).emit(ACTIONS.ADD_PEER, {
+        peerId: socket.id,
+        createOffer: false,
+        user,
+      });
+      socket.emit(ACTIONS.ADD_PEER, {
+        peerId: clientId,
+        createOffer: true,
+        user: socketUserMap[clientId],
+      });
     });
+    socket.join(roomId);
+  });
 
-    socket.on(ACTIONS.RELAY_ICE, ({ peerId, icecandidate }) => {
-        io.to(peerId).emit(ACTIONS.ICE_CANDIDATE, {
-            peerId: socket.id,
-            icecandidate,
-        });
+  socket.on(ACTIONS.RELAY_ICE, ({ peerId, icecandidate }) => {
+    io.to(peerId).emit(ACTIONS.ICE_CANDIDATE, {
+      peerId: socket.id,
+      icecandidate,
     });
+  });
 
-    socket.on(ACTIONS.RELAY_SDP, ({ peerId, sessionDescription }) => {
-        io.to(peerId).emit(ACTIONS.SESSION_DESCRIPTION, {
-            peerId: socket.id,
-            sessionDescription,
-        });
+  socket.on(ACTIONS.RELAY_SDP, ({ peerId, sessionDescription }) => {
+    io.to(peerId).emit(ACTIONS.SESSION_DESCRIPTION, {
+      peerId: socket.id,
+      sessionDescription,
     });
+  });
 
-    socket.on(ACTIONS.MUTE, ({ roomId, userId }) => {
-        console.log('Mute', userId)
-        const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
-        clients.forEach((clientId) => {
-            io.to(clientId).emit(ACTIONS.MUTE, {
-                peerId: socket.id,
-                userId,
-            });
-        });
+  socket.on(ACTIONS.MUTE, ({ roomId, userId }) => {
+    console.log('Mute', userId)
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+    clients.forEach((clientId) => {
+      io.to(clientId).emit(ACTIONS.MUTE, {
+        peerId: socket.id,
+        userId,
+      });
     });
+  });
 
-    socket.on(ACTIONS.UNMUTE, ({ roomId, userId }) => {
-        const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
-        clients.forEach((clientId) => {
-            io.to(clientId).emit(ACTIONS.UNMUTE, {
-                peerId: socket.id,
-                userId,
-            });
-        });
+  socket.on(ACTIONS.UNMUTE, ({ roomId, userId }) => {
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+    clients.forEach((clientId) => {
+      io.to(clientId).emit(ACTIONS.UNMUTE, {
+        peerId: socket.id,
+        userId,
+      });
     });
+  });
 
-    socket.on(ACTIONS.MUTE_INFO, ({ userId, roomId, isMute }) => {
-        const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
-        clients.forEach((clientId) => {
-            if (clientId !== socket.id) {
-                console.log('mute info');
-                io.to(clientId).emit(ACTIONS.MUTE_INFO, {
-                    userId,
-                    isMute,
-                });
-            }
+  socket.on(ACTIONS.MUTE_INFO, ({ userId, roomId, isMute }) => {
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+    clients.forEach((clientId) => {
+      if (clientId !== socket.id) {
+        console.log('mute info');
+        io.to(clientId).emit(ACTIONS.MUTE_INFO, {
+          userId,
+          isMute,
         });
+      }
     });
+  });
 
-    const leaveRoom = () => {
-        const { rooms } = socket;
-        Array.from(rooms).forEach((roomId) => {
-            const clients = Array.from(
-                io.sockets.adapter.rooms.get(roomId) || []
-            );
-            clients.forEach((clientId) => {
-                io.to(clientId).emit(ACTIONS.REMOVE_PEER, {
-                    peerId: socket.id,
-                    userId: socketUserMap[socket.id]?.id,
-                });
-
-                // socket.emit(ACTIONS.REMOVE_PEER, {
-                //     peerId: clientId,
-                //     userId: socketUserMap[clientId]?.id,
-                // });
-            });
-            socket.leave(roomId);
+  const leaveRoom = () => {
+    const { rooms } = socket;
+    Array.from(rooms).forEach((roomId) => {
+      const clients = Array.from(
+        io.sockets.adapter.rooms.get(roomId) || []
+      );
+      clients.forEach((clientId) => {
+        io.to(clientId).emit(ACTIONS.REMOVE_PEER, {
+          peerId: socket.id,
+          userId: socketUserMap[socket.id]?.id,
         });
-        delete socketUserMap[socket.id];
-    };
 
-    socket.on(ACTIONS.LEAVE, leaveRoom);
+        // socket.emit(ACTIONS.REMOVE_PEER, {
+        //     peerId: clientId,
+        //     userId: socketUserMap[clientId]?.id,
+        // });
+      });
+      socket.leave(roomId);
+    });
+    delete socketUserMap[socket.id];
+  };
 
-    socket.on('disconnecting', leaveRoom);
+  socket.on(ACTIONS.LEAVE, leaveRoom);
+
+  socket.on('disconnecting', leaveRoom);
 });
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
